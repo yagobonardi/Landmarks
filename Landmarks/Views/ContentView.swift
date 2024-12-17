3de14c9b39839db9b0271ac35c17d70aa891e566
yagobonardi <yagobonardi@gmail.com>
2024-12-17 15:16:58 -0300

    subindo tutorial para repo

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+        .environment(ModelData())
+}
---
+++ Landmarks/Views/Helper/CircleImage.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    
+    var body: some View {
+        image
+            .clipShape(.circle)
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage(image: Image("turtlerock"))
+}
